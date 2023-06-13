3999cc68547de6cd95965ac8db213e3094d70fb1
Neil-140301 <alvares.1@iitj.ac.in>
2023-06-13 21:34:04 +0530

    ui | added loading and empty home page

--- src/app/new/Loading.tsx
+++ src/app/new/Loading.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function Loading() {
   return (
-    <div>Loading</div>
-  )
+    <div className={"flex flex-col items-center justify-center gap-4"}>
+      <Image width={400} height={400} src={"/wait.png"} alt={"loading"} />
+      <span className={"italic text-slate-400"}>Loading ...</span>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image';
+import Image from "next/image";
+import React from "react";
 
 export default function Home() {
-	return (
-		<main className="flex flex-col items-center justify-between p-24"></main>
-	);
+  return (
+    <div className="flex flex-col gap-5 p-10 pt-6">
+      <h1 className="text-2xl font-semibold text-gray-600">Blogs</h1>
+      <div className={"flex h-96 flex-col items-center justify-center"}>
+        <Image width={400} height={400} src={"/empty.png"} alt={"empty"} />
+        <span className={"italic text-slate-400"}>No Posts yet ...</span>
+      </div>
+    </div>
+  );
 }
